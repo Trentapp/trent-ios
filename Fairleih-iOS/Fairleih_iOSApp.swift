0a8bce33a0094ae31d477419e59a3323883bfa39
Fynn <fynnkiwitt@yahoo.de>
2021-05-11 09:35:57 +0200

    Initial Commit

---
+++ Fairleih-iOS/Fairleih_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fairleih_iOSApp.swift
+//  Fairleih-iOS
+//
+//  Created by Fynn Kiwitt on 11.05.21.
+//
+
+import SwiftUI
+
+@main
+struct Fairleih_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
