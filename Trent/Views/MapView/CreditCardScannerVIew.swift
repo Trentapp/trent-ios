fa74dc0d268d479dd65864f4c750cb1469ccce92
Fynn <fynnkiwitt@yahoo.de>
2021-07-29 13:47:18 +0200

    added credit card scanner view

---
+++ Trent/Views/MapView/CreditCardScannerVIew.swift
@@ -0,0 +1,15 @@
+//
+//  CreditCardScannerVIew.swift
+//  Trent
+//
+//  Created by Fynn Kiwitt on 29.07.21.
+//
+
+import SwiftUI
+
+struct CreditCardScannerView: View {
+    var body: some View {
+        Text("credit card scanner")
+            .navigationBarTitle("Add credit card", displayMode: .large)
+    }
+}
